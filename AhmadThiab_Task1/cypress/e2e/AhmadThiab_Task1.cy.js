d8fff49f200e04cb3462a8c3574b86f56da3bd0f
Ahmad Diab <aahhmaadd33@gmail.com>
2025-11-28 15:53:24 +0200

    Complete Task 1: Add Cypress selectors solution

---
+++ AhmadThiab_Task1/cypress/e2e/AhmadThiab_Task1.cy.js
@@ -0,0 +1,34 @@
+// task1.cy.js
+/// <reference types="cypress" />
+
+describe('Ahmad Thiab Task 1 ', () => {
+    
+
+    it('Selector Task 1', () => {
+        
+
+        cy.visit('https://demo.productionready.io/#/register')
+
+    
+        cy.wait(1000)
+
+        cy.get('h1.text-xs-center').should('be.visible').and('contain', 'Sign up')
+
+     
+        cy.get('input[placeholder="Username"]') // .type('')
+
+        cy.get('input[placeholder="Email"]') // .type('')
+        //cy.get('fieldset:nth-child(2) input') //.type('')
+
+        cy.get('input[type="password"]') // .type('')
+        
+   
+        cy.get('button[type="submit"].btn-primary').should('be.visible').and('contain', 'Sign up')
+
+        cy.get('a.navbar-brand').should('be.visible').and('contain', 'conduit')
+
+        cy.get('a.nav-link').contains('Home').should('be.visible')
+        
+     
+    })
+})
