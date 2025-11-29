27d5113d751623dad9f42fdb158ad6b31770f123
Ahmad Diab <aahhmaadd33@gmail.com>
2025-11-29 12:17:59 +0200

    Edite

--- AhmadThiab_Task1/cypress/e2e/AhmadThiab_Task1.cy.js
+++ AhmadThiab_Task1/cypress/e2e/AhmadThiab_Task1.cy.js
@@ -12,18 +12,19 @@ describe('Ahmad Thiab Task 1 ', () => {
     
         cy.wait(1000)
 
-        cy.get('h1.text-xs-center').should('be.visible').and('contain', 'Sign up')
+        cy.get('h1.text-xs-center').should('be.visible').and('contain', 'Sign up') //.click()
 
      
         cy.get('input[placeholder="Username"]') // .type('')
 
         cy.get('input[placeholder="Email"]') // .type('')
+
         //cy.get('fieldset:nth-child(2) input') //.type('')
 
         cy.get('input[type="password"]') // .type('')
         
    
-        cy.get('button[type="submit"].btn-primary').should('be.visible').and('contain', 'Sign up')
+        cy.get('button[type="submit"].btn-primary').should('be.visible').and('contain', 'Sign up') //.click()
 
         cy.get('a.navbar-brand').should('be.visible').and('contain', 'conduit')
 
